561b85325e5a2a4a5196e4cb66201d51181caa2a
eurialo <federico.fazzi@gmail.com>
2014-10-11 07:59:35 +0200

    Initialize

---
+++ rapidtor.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2012 Federico Fazzi - http://deftcode.ninja
+# rapidtor.py - timedelay id changer in Tor network
+#
+# deftcode ~ $ python rapidtor.py
+# Authentication....OK!
+# >> Changed identity in 5 second/s..OK!
+# >> Changed identity in 5 second/s..OK!
+# >> Changed identity in 5 second/s..OK!
+# ..
+
+import os, sys
+import socket, signal, time
+
+class RapidTor:
+
+	host = 'localhost1'		# server tor host
+	port = 9051				# need ControlPort enabled
+	passwd = 'torpasswd'	# your tor server password
+	delay = 15				# change my identity after 5 secs..
+	enable_cache = 1 		# 1=enable, 0=disable
+
+	def __init__(self, host = None, port = None, passwd = None, delay = None, enable_cache = None):
+		if host is not None:
+			self.host = host
+			self.port = port
+			self.passwd = passwd
+			self.delay = delay
+			self.enable_cache = enable_cache
+
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+			self.sock.connect((self.host, self.port))
+			return True
+		except Exception, e:
+			sys.stdout.write("%s: %s\n" % (self.host, e))
+
+	def signal_newnym(self):
+		sys.stdout.write('Authentication..')
+		self.sock.send("AUTHENTICATE \"%s\"\r\n" % self.passwd)
+		self.data = self.sock.recv(1024)
+
+		if not self.data.find('250 OK'):
+			sys.stdout.write("..OK!\n")
+		else:
+			sys.stdout.write("..NOT\n")
+			return -1
+
+		while len(self.data):
+			sys.stdout.write(">> Changed identity in %d second/s" % self.delay)
+			time.sleep(self.delay)
+			self.sock.send("SIGNAL NEWNYM\r\n")
+			self.data = self.sock.recv(1024)
+			
+			if self.enable_cache == 1:
+				self.signal_cleardnscache()
+
+			sys.stdout.write('..OK!\n')
+
+	def signal_cleardnscache(self):
+		self.sock.send("SIGNAL CLEARDNSCACHE\r\n")
+		self.data = self.sock.recv(1024)
+
+if __name__ == "__main__":
+	rapidtor = RapidTor('localhost2', 9051, 'torpasswd', 15, 1)
+
+	if rapidtor is True:
+		rapidtor.signal_newnym()
