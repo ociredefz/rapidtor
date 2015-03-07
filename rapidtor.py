ab123d70cef2f4ceb8955cb5900345438159fe1d
eurialo <federico.fazzi@gmail.com>
2015-03-07 10:21:34 +0100

    Added howto comment, fixed wrong constructor return value

--- rapidtor.py
+++ rapidtor.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python
 #
-# Copyright (C) 2012 Federico Fazzi - http://deftcode.ninja
+# Copyright (C) 2012-2015 Federico Fazzi - http://deftcode.ninja
 # rapidtor.py - timedelay id changer in Tor network
+#
+# First generate hashed control password:
+# deftcode ~ $ tor --quiet --hash-password torpasswd
+# 16:C9B4152185E88628607DE42FA83F01801BD332C8D91ACC6B29CC863F4A
+# 
+# Edit ~/.torrc configuration file and uncomment these lines
+# then change the HashedControlPassword with previously generated:
+# --
+# RunAsDaemon 1
+# ControlPort 9051
+# HashedControlPassword 16:C9B4152185E88628607DE42FA83F01801BD332C8D91ACC6B29CC863F4A
+# --
+#
+# Force tor daemon to use your custom configuration file:
+# deftcode ~ $ tor --quiet -f ~/.torrc
+# Mar 07 09:46:12.543 [notice] Tor v0.2.4.20 (git-0d50b03673670de6).
+# Mar 07 09:46:12.543 [notice] Read configuration file "/home/eurialo/.torrc".
+# Mar 07 09:46:12.550 [notice] Opening Socks listener on 127.0.0.1:9050
+# Mar 07 09:46:12.550 [notice] Opening Control listener on 127.0.0.1:9051
+# Mar 07 09:46:12.550 [notice] Opening OR listener on 0.0.0.0:9001
 #
 # deftcode ~ $ python rapidtor.py
 # Authentication....OK!
@@ -15,7 +35,7 @@ import socket, signal, time
 
 class RapidTor:
 
-	host = 'localhost1'		# server tor host
+	host = 'localhost'		# server tor host
 	port = 9051				# need ControlPort enabled
 	passwd = 'torpasswd'	# your tor server password
 	delay = 15				# change my identity after 5 secs..
@@ -29,6 +49,7 @@ class RapidTor:
 			self.delay = delay
 			self.enable_cache = enable_cache
 
+	def connect(self):
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try:
 			self.sock.connect((self.host, self.port))
@@ -42,9 +63,9 @@ class RapidTor:
 		self.data = self.sock.recv(1024)
 
 		if not self.data.find('250 OK'):
-			sys.stdout.write("..OK!\n")
+			sys.stdout.write("..SUCCESS!\n")
 		else:
-			sys.stdout.write("..NOT\n")
+			sys.stdout.write("..BAD!\n" + self.data)
 			return -1
 
 		while len(self.data):
@@ -63,7 +84,7 @@ class RapidTor:
 		self.data = self.sock.recv(1024)
 
 if __name__ == "__main__":
-	rapidtor = RapidTor('localhost2', 9051, 'torpasswd', 15, 1)
+	rapidtor = RapidTor('localhost', 9051, 'torpasswd', 15, 1)
 
-	if rapidtor is True:
+	if rapidtor.connect() is True:
 		rapidtor.signal_newnym()
